5ba75488b5f1294178aa1d866b7a429fcc1b102d
Àngel Mariages <angel.mariages@marfeel.com>
2018-06-02 20:53:50 +0200

    Fix lint

--- src/js/background.js
+++ src/js/background.js
@@ -1,44 +1,50 @@
 const toggle = () => {
-  const checkboxes = document.querySelectorAll("input[type='checkbox']");
-  checkboxes.forEach((checkbox) => {
-    checkbox.checked = true;
-  });
+	const checkboxes = document.querySelectorAll('input[type=\'checkbox\']');
+
+	checkboxes.forEach((checkbox) => {
+		checkbox.checked = true;
+	});
 };
 
 const setAsDefaultEnv = (env) => {
 	const proOption = document.querySelector(`option[value='${env}']`);
-	if(proOption) proOption.selected = true;
-};
 
-const ajax = (url, successCallback, errorCallback, timeout, xml) => {
-    const request = new XMLHttpRequest();
-    request.timeout = timeout || 10000;
-    request.open("GET", url);
-
-    setReadyStateChange(request, successCallback, errorCallback, xml);
-    request.send();
+	if (proOption) {
+		proOption.selected = true;
+	}
 };
 
 const setReadyStateChange = (request, successCallback, errorCallback, xml) => {
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status === 200) {
-                successCallback && successCallback(!xml ? request.responseText : request.responseXML);
-            } else {
-                errorCallback && errorCallback(request);
-            }
-        }
-    };
+	request.onreadystatechange = () => {
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				successCallback && successCallback(!xml ? request.responseText : request.responseXML);
+			} else {
+				errorCallback && errorCallback(request);
+			}
+		}
+	};
 };
 
-ajax(chrome.extension.getURL("/json/config.json"), function(response) {
-  const jsonConfig = JSON.parse(response);
-  const URLS = jsonConfig.urls;
-  const SELECT_OPTION = jsonConfig.defaultOption;
-  if(URLS.find((url) => window.location.href.search(url) >= 0)) {
-    toggle();
-    setAsDefaultEnv(SELECT_OPTION);
-  }
+const ajax = (url, successCallback, errorCallback, timeout, xml) => {
+	const request = new XMLHttpRequest();
+
+	request.timeout = timeout || 10000;
+	request.open('GET', url);
+
+	setReadyStateChange(request, successCallback, errorCallback, xml);
+	request.send();
+};
+
+ajax(chrome.extension.getURL('/json/config.json'), function(response) {
+	const jsonConfig = JSON.parse(response);
+	const URLS = jsonConfig.urls;
+	const SELECT_OPTION = jsonConfig.defaultOption;
+
+	if (URLS.find((url) => window.location.href.search(url) >= 0)) {
+		toggle();
+		setAsDefaultEnv(SELECT_OPTION);
+	}
 });
 
 
