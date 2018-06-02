8d3219e12796751829a55e744624bbb29098aa52
Àngel Mariages <angel.mariages@gmail.com>
2018-06-03 00:27:56 +0200

    Refactor code

--- src/js/background.js
+++ src/js/background.js
@@ -14,37 +14,29 @@ const setAsDefaultEnv = (env) => {
 	}
 };
 
-const setReadyStateChange = (request, successCallback, errorCallback, xml) => {
-	request.onreadystatechange = () => {
-		if (request.readyState === 4) {
-			if (request.status === 200) {
-				successCallback && successCallback(!xml ? request.responseText : request.responseXML);
-			} else {
-				errorCallback && errorCallback(request);
+const getConfigFile = (fileURL, successCallback, errorCallback) => {
+	const request = fetch(fileURL);
+
+	successCallback && request.then(successCallback);
+	errorCallback && request.catch(errorCallback);
+};
+
+const parseResponse = (response) => {
+	const contentType = response.headers.get('content-type');
+
+	if (contentType && contentType.indexOf('application/json') !== -1) {
+		response.json().then(jsonConfig => {
+			const URLS = jsonConfig.urls;
+			const SELECT_OPTION = jsonConfig.defaultOption;
+
+			if (URLS.find((url) => window.location.href.search(url) >= 0)) {
+				toggle();
+				setAsDefaultEnv(SELECT_OPTION);
 			}
-		}
-	};
-};
-
-const ajax = (url, successCallback, errorCallback, timeout, xml) => {
-	const request = new XMLHttpRequest();
-
-	request.timeout = timeout || 10000;
-	request.open('GET', url);
-
-	setReadyStateChange(request, successCallback, errorCallback, xml);
-	request.send();
-};
-
-ajax(chrome.extension.getURL('/json/config.json'), function(response) {
-	const jsonConfig = JSON.parse(response);
-	const URLS = jsonConfig.urls;
-	const SELECT_OPTION = jsonConfig.defaultOption;
-
-	if (URLS.find((url) => window.location.href.search(url) >= 0)) {
-		toggle();
-		setAsDefaultEnv(SELECT_OPTION);
+		});
 	}
-});
+};
+
+getConfigFile(chrome.runtime.getURL('/json/config.json'), parseResponse);
 
 
